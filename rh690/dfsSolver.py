bb9c345ae6e0b737075fa8b4352899e21ca389fd
ReallyMonk <hourunlinxa@gmail.com>
2020-04-19 20:31:18 -0400

    'rh690'

---
+++ rh690/dfsSolver.py
@@ -0,0 +1,127 @@
+import random
+
+
+class bfsSolver():
+    def __init__(self):
+        print('create solver')
+        pass
+
+    def update_info(self, head, tail, body, food, width):
+        self.head = head
+        self.tail = tail
+        self.body = body
+        self.food = food
+        self.width = width
+        self.walls = []
+        self.set_walls()
+
+    def set_walls(self):
+        w = self.width
+        self.walls.extend(range(w))
+        self.walls.extend(range((w - 1) * w, w * w))
+
+        for i in range(1, w - 1):
+            self.walls.append(i * w)
+            self.walls.append((i + 1) * w - 1)
+        return
+
+    def connect(self, a):
+        # the map is store in 1-D array
+        # so the connected nodes can be decided by index
+        # if the other nodes is not wall
+        # then nodes n is connected to
+        # node n-1, n+1, n-width, n+width
+        w = self.width
+        walls = self.walls
+        connect_node = []
+        _map = self.body
+        if a - 1 not in walls and _map[a - 1] == 0:
+            connect_node.append(a - 1)
+
+        if a + 1 not in walls and _map[a + 1] == 0:
+            connect_node.append(a + 1)
+
+        if a + w not in walls and _map[a + w] == 0:
+            connect_node.append(a + w)
+
+        if a - w not in walls and _map[a - w] == 0:
+            connect_node.append(a - w)
+
+        return connect_node
+
+    def bfs(self, head, end):
+        path = [None for i in range(self.width**2)]
+        path[head] = head
+        queue = [head]
+
+        while queue and not path[end]:
+            tmp = []
+            tmp.extend(queue)
+            for node in tmp:
+                # print('current node: ', node)
+                # print('cur queue ', queue)
+                con_nodes = self.connect(node)
+
+                # add new node in to the queue
+                # set the last node in path
+                for new_node in con_nodes:
+                    if not path[new_node]:
+                        queue.append(new_node)
+                        path[new_node] = node
+
+                # pop out queue
+                queue.remove(node)
+                # print('cur que ', queue)
+
+        #print('out')
+        #print('food', self.food, ' head ', self.head)
+        if not path[end]:
+            return False
+        else:
+            s_path = []
+            pre = end
+            while head != pre:
+                s_path.append(pre)
+                pre = path[pre]
+
+            # s_path.append(pre)
+            # print(s_path)
+        return s_path
+
+    def find_path(self):
+        # a stratgy to enhance the program
+        # if snake can reach the food, go for it
+        # if snkae can not reach the food, trace the tail
+        # if not, go where every you can
+        r_path = self.bfs(self.head, self.food)
+
+        if r_path:
+            pass
+        else:
+            tail_nbr = self.connect(self.tail)
+            if tail_nbr:
+                for nbr in tail_nbr:
+                    r_path = self.bfs(self.head, nbr)
+                    if r_path:
+                        break
+                    else:
+                        dirs = self.connect(self.head)
+                        #print(dirs)
+                        if not dirs:
+                            r_path = []
+                        else:
+                            seed = random.randint(0, len(dirs)-1)
+                            r_path = [dirs[seed]]
+        
+        return r_path
+
+    '''
+        if not r_path:
+            return []
+        else:
+            # path = [i for i in reversed(r_path)]
+            # return false means try to go where every snake can
+       ''' 
+
+        
+
---
+++ rh690/snakeDemo.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+@Author: Jin X
+@Date: 2020-04-09 15:01:42
+@LastEditTime: 2020-04-13 12:28:25
+'''
+from random import sample
+import math
+import numpy as np
+import os
+import time
+import dfsSolver as bfs
+# block_num = 8
+
+
+class Snake():
+    def __init__(self, block_num, initBody=None):
+        self.dir = -1
+        self.bn2 = block_num + 2
+        self.body = [0] * self.bn2**2
+        # initBody = [31, 41, 42, 43]
+        self.len = 0
+        if not initBody:
+            halfN = len(self.body) // 2
+            initBody = [halfN + i for i in range(-4, -1)]
+        for i in range(len(initBody) - 1, 0, -1):
+            self.body[initBody[i]] = initBody[i - 1]
+            self.len += 1
+        for i in range(self.bn2**2):
+            x, y = i % self.bn2, i // self.bn2
+            if x == 0 or x == (self.bn2 - 1) or y == 0 or y == (self.bn2 - 1):
+                self.body[i] = 1
+        self.head = self.body[initBody[0]] = initBody[0]
+        self.tail = initBody[-1]
+        self.oldtail = initBody[-1] + 1
+        self.path = []
+        print(self)
+
+        # create a slover
+        self.solver = bfs.bfsSolver()
+        self.generateFood()
+        self.TEMP = (initBody, self.food, self.path)
+
+    def pass_info(self):
+        self.solver.update_info(self.head, self.tail, self.body.copy(),
+                                self.food, self.bn2)
+        return
+
+    def move(self):
+        #self.pass_info()
+        if not self.changDir():
+            return False
+        oldhead = self.head
+        self.head += self.dir
+        if self.body[self.head]:
+            print('lost!')
+            return False
+            # pass
+        self.body[self.head] = self.body[oldhead] = self.head
+        if self.head != self.food:
+            self.oldtail = self.tail
+            self.body[self.tail], self.tail = 0, self.body[self.tail]
+            return self.head, self.oldtail
+        else:
+            #print('score++')
+            self.len += 1
+            self.generateFood()
+            #print('food', self.food)
+            return self.head, None, self.food
+        return True
+
+    def up(self):
+        self.dir = -self.bn2
+        self.move()
+
+    def down(self):
+        self.dir = self.bn2
+        self.move()
+
+    def left(self):
+        self.dir = -1
+        self.move()
+
+    def right(self):
+        self.dir = +1
+        self.move()
+
+    def changDir(self):
+        self.pass_info()
+        self.path = self.solver.find_path()
+
+        if len(self.path):
+            self.dir = self.path.pop() - self.head
+        else:
+            print('out of path')
+            return False
+        return True
+
+    def __str__(self):
+        num = int(math.sqrt(len(self.body)))
+        res = ''
+        for i in range(num):
+            temp = self.body[i * num:(i + 1) * num]
+            res += str(temp)[1:-1] + "\n"
+        return res
+
+    def generateFood(self):
+        grid_size = len(self.body)
+        empty = [i for i in range(grid_size) if not self.body[i]]
+        if not empty:
+            print('win')
+            return
+        self.food = sample(empty, 1)[0]
+        #print('food at:{}'.format(self.food))
+
+        #self.move()
+        #print('path', self.path)
+        #print(self)
+        #input()
+
+
+if __name__ == '__main__':
+    all_time = []
+    score = []
+
+    for i in range(8, 17):
+        tmp_time = []
+        tmp_score = []
+        for k in range(10):
+            print('current map size is ', i)
+            # create snake game
+            snake = Snake(i)
+            # play
+            t0 = time.time()
+            while snake.move():
+                pass
+
+            t1 = time.time()
+
+            tmp_time.append(t1 - t0)
+            tmp_score.append(snake.len)
+
+        all_time.append(np.mean(tmp_time))
+        score.append(np.mean(tmp_score))
+
+    print(all_time)
+    print(score)
+
+'''
+if __name__ == "__main__":
+    snake = Snake(10)
+
+    while snake.move():
+        pass'''
